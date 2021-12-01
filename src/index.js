1e9ce4cd62d9a6f72e6e6da329abd9b6cd8c8b40
shn's works <53104737+shnlog@users.noreply.github.com>
2021-12-01 22:19:08 +0900

    コンポーネントの使い方を知る

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom"; // HTMLにコンポーネントを反映するので、こちらを記載する。
-
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
