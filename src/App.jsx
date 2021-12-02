242ce61551a6608cd0d1e37294db7777bc9b83e8
shn's works <53104737+shnlog@users.noreply.github.com>
2021-12-02 17:35:35 +0900

    Propsを知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import ColorfulMassage from "./components/ColorfulMassage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
+      <ColorfulMassage color="blue">お元気ですか？</ColorfulMassage>
+      <ColorfulMassage color="pink">元気です！</ColorfulMassage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
